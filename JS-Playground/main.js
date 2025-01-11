c731b922de6eef0998a8348ac3f8a742c622d45b
coollondos <londos2021@gmail.com>
2025-01-11 20:21:07 +0000

    extra features

--- JS-Playground/main.js
+++ JS-Playground/main.js
@@ -3,10 +3,13 @@ function rizz() {
 count++;
   if (count == 1) {
   alert("Click me again!");
+  getElementById('test').innerHTML = "Click me again!";
 } else if (count == 2) {
   alert("Yippee");
+  getElementById('test').innerHTML = "Yippee";
 } else {
   alert("e");
+  getElementById('test').innerHTML = "e";
 }
 
 }
