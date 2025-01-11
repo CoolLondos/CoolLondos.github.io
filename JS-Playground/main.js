2fd320dc3ea1183fb17068cc8fd5bb02b8e17ba4
coollondos <londos2021@gmail.com>
2025-01-11 20:27:10 +0000

    more extra features and fixes

--- JS-Playground/main.js
+++ JS-Playground/main.js
@@ -3,13 +3,16 @@ function rizz() {
 count++;
   if (count == 1) {
   alert("Click me again!");
-  getElementById('test').innerHTML = "Click me again!";
+  document.getElementById('test').innerHTML = "Click me again!";
+  document.getElementById('test').style.fontsize = "20px";
 } else if (count == 2) {
   alert("Yippee");
-  getElementById('test').innerHTML = "Yippee";
+  document.getElementById('test').innerHTML = "Yippee";
+  document.getElementById('test').style.fontsize = "24px";
 } else {
   alert("e");
-  getElementById('test').innerHTML = "e";
+  document.getElementById('test').innerHTML = "e";
+  document.getElementById('test').style.fontsize = "30px";
 }
 
 }
