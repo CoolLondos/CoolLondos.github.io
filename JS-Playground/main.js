6faed238a470d657fd95cef310c9769c118ea1bc
coollondos <londos2021@gmail.com>
2025-03-15 18:43:16 +0000

    updated A LOT of the website :)

--- JS-Playground/main.js
+++ JS-Playground/main.js
@@ -1,18 +1,10 @@
 var count = 0;
-function rizz() {
-count++;
-  if (count == 1) {
-  alert("Click me again!");
-  document.getElementById('test').innerHTML = "Click me again!";
-  document.getElementById('test').style.fontsize = "20px";
-} else if (count == 2) {
-  alert("Yippee");
-  document.getElementById('test').innerHTML = "Yippee";
-  document.getElementById('test').style.fontsize = "24px";
-} else {
-  alert("e");
-  document.getElementById('test').innerHTML = "e";
-  document.getElementById('test').style.fontsize = "30px";
-}
+function rizzup() {
+count = count + 1;
+document.getElementById('test').innerHTML = "Number: " + count;
+};
 
-}
+function rizzdown() {
+count = count - 1;
+document.getElementById('test').innerHTML = "Number: " + count;
+};
