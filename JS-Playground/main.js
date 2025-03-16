182bf559faa9623963110d6f72daf8071ebf2ded
coollondos <londos2021@gmail.com>
2025-03-16 15:12:17 +0000

    more functions

--- JS-Playground/main.js
+++ JS-Playground/main.js
@@ -1,10 +1,24 @@
 var count = 0;
 function rizzup() {
 count = count + 1;
-document.getElementById('test').innerHTML = "Number: " + count;
+reloadnumber()
 };
 
 function rizzdown() {
 count = count - 1;
+reloadnumber()
+};
+
+function rizzx2() {
+count = count * 2;
+reloadnumber()
+};
+
+function rizzhalf2() {
+count = count / 2;
+reloadnumber()
+};
+
+function reloadnumber() {
 document.getElementById('test').innerHTML = "Number: " + count;
 };
