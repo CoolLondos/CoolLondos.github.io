ee438b5072ea6f9be41d2d08067cf8f2704e6856
Londos <londos2021@gmail.com>
2025-06-03 19:11:20 +0100

    hope

---
+++ other/nfc-funny/main.js
@@ -0,0 +1,5 @@
+function ble() {
+  document.getElementById('sound1').play();
+  document.getElementById('hideme').style = "width: 100%; height: auto; display: none;";
+  document.getElementById('text').style = "width: 100%; height: auto;";
+}
