2f58c2a33c8045964796873964e6dfb70d5ddef6
Londos <londos2021@gmail.com>
2025-06-10 08:37:02 +0100

    Update main.js

--- other/nfc-funny/main.js
+++ other/nfc-funny/main.js
@@ -1,5 +1,5 @@
 function ble() {
   document.getElementById('sound1').play();
-  document.getElementById('hideme').style = "width: 100%; height: auto; display: none;";
-  document.getElementById('text').style = "width: 100%; height: auto;";
+  document.getElementById('hideme').style = "display: none;";
+  document.getElementById('text').style = "text-align: center; font-size:100px;";
 }
